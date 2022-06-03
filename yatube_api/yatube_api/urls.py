bfe8dde5502ce5c56b4b38d1a579c4edbcb526c9
Валентин Краснов <valkred@gmail.com>
2022-06-03 16:08:01 +0300

    SPRINT_9 API FINAL

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
@@ -15,7 +13,6 @@ v1_router.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(v1_router.urls)),
     path('api/v1/', include(v1_router.urls)),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.jwt')),
@@ -26,10 +23,3 @@ urlpatterns = [
     ),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(
-#         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-#     )
-#     urlpatterns += static(
-#         settings.STATIC_URL, document_root=settings.STATIC_ROOT
-#     )
